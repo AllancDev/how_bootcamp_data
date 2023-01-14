124cf575732ef1ed701958b4ba2e46c816f46b25
Allan Camargo <allan.cdev@gmail.com>
2023-01-13 23:46:12 -0300

    fix: updated file ingestor to ingestors

--- apis.py
+++ apis.py
@@ -4,19 +4,18 @@ from abc import ABC, abstractmethod
 import logging
 import ratelimit
 import requests
-
-from backoff import on_exception, expo
-
 from backoff import on_exception, expo
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
+
 class MercadoBitcoinApi(ABC):
+
     def __init__(self, coin: str) -> None:
         self.coin = coin
-        self.base_endpoint = 'https://www.mercadobitcoin.net/api'
-    
+        self.base_endpoint = "https://www.mercadobitcoin.net/api"
+
     @abstractmethod
     def _get_endpoint(self, **kwargs) -> str:
         pass
@@ -24,15 +23,16 @@ class MercadoBitcoinApi(ABC):
     @on_exception(expo, ratelimit.exception.RateLimitException, max_tries=10)
     @ratelimit.limits(calls=29, period=30)
     @on_exception(expo, requests.exceptions.HTTPError, max_tries=10)
-    def _get_data(self, **kwargs) -> dict:
+    def get_data(self, **kwargs) -> dict:
         endpoint = self._get_endpoint(**kwargs)
-        logger.info(f"Getting data from endpoint {endpoint}")
+        logger.info(f"Getting data from endpoint: {endpoint}")
         response = requests.get(endpoint)
         response.raise_for_status()
         return response.json()
 
+
 class DaySummaryApi(MercadoBitcoinApi):
-    type = 'day-summary'
+    type = "day-summary"
 
     def _get_endpoint(self, date: datetime.date) -> str:
         return f"{self.base_endpoint}/{self.coin}/{self.type}/{date.year}/{date.month}/{date.day}"
@@ -46,14 +46,14 @@ class TradesApi(MercadoBitcoinApi):
     def _get_endpoint(self, date_from: datetime.datetime = None, date_to: datetime.datetime = None) -> str:
         if date_from and not date_to:
             unix_date_from = self._get_unix_epoch(date_from)
-            endpoint = f"{self.base_endpoint}/{self.coin}/{self.type}/{unix_date_from}"
+            endpoint = f'{self.base_endpoint}/{self.coin}/{self.type}/{unix_date_from}'
         elif date_from and date_to:
             if date_from > date_to:
                 raise RuntimeError("date_from cannot be greater than date_to")
             unix_date_from = self._get_unix_epoch(date_from)
             unix_date_to = self._get_unix_epoch(date_to)
-            endpoint = f"{self.base_endpoint}/{self.coin}/{self.type}/{unix_date_from}/{unix_date_to}"
+            endpoint = f'{self.base_endpoint}/{self.coin}/{self.type}/{unix_date_from}/{unix_date_to}'
         else:
-            endpoint = f"{self.base_endpoint}/{self.coin}/{self.type}"
-        
+            endpoint = f'{self.base_endpoint}/{self.coin}/{self.type}'
+
         return endpoint
